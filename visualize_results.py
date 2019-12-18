c0cc8ec44c9e54d9e29e18c1e027e56bc98a8697
eveem <even_narak@hotmail.coml>
2019-12-18 19:14:37 +0700

    add cropped image

--- visualize_results.py
+++ visualize_results.py
@@ -26,8 +26,8 @@ def viz_textbb(text_im, charBB_list, wordBB, alpha=1.0):
     plt.close(1)
     plt.figure(1)
     plt.imshow(text_im)
-    img = Image.fromarray(text_im)
-    img.show()
+    # img = Image.fromarray(text_im)
+    # img.show()
     # plt.hold(True)
     H, W = text_im.shape[:2]
 
@@ -54,12 +54,15 @@ def viz_textbb(text_im, charBB_list, wordBB, alpha=1.0):
     plt.gca().set_ylim([H - 1, 0])
     plt.show(block=False)
 
-
 def main(db_fname):
     dt = h5py.special_dtype(vlen=str)
     db = h5py.File(db_fname, "r")
     dsets = sorted(db["data"].keys())
     print("total number of images : ", colorize(Color.RED, len(dsets), highlight=True))
+    
+    counter = 0
+    f = open('gt.txt', 'w')
+    output_path = 'results/cropped/'
     for k in dsets:
         rgb = db["data"][k][...]
         charBB = db["data"][k].attrs["charBB"]
@@ -67,15 +70,32 @@ def main(db_fname):
         txt = db["data"][k].attrs["txt"]
         txt = [n.decode("utf-8", "ignore") for n in txt]
 
-        viz_textbb(rgb, [charBB], wordBB)
-        print("image name        : ", colorize(Color.RED, k, bold=True))
-        print("  ** no. of chars : ", colorize(Color.YELLOW, charBB.shape[-1]))
-        print("  ** no. of words : ", colorize(Color.YELLOW, wordBB.shape[-1]))
-        print("  ** text         : ", colorize(Color.GREEN, txt))
+        # viz_textbb(rgb, [charBB], wordBB)
+        # get cropped images
+        img = Image.fromarray(rgb)
+        
+        for i in range(wordBB.shape[-1]):
+            bb = wordBB[:,:,i]
+            left = min(bb[0])
+            up = min(bb[1])
+            right = max(bb[0])
+            lower = max(bb[1])
 
-        if "q" in input("next? ('q' to exit) : "):
-            break
+            cropped = img.crop((left, up, right, lower))
+            filename = str(counter) + '.jpg'
+            cropped.save(output_path + filename)
+            counter += 1
+            f.write(filename + '\t' + txt[i] + '\n')
+
+        # print("image name        : ", colorize(Color.RED, k, bold=True))
+        # print("  ** no. of chars : ", colorize(Color.YELLOW, charBB.shape[-1]))
+        # print("  ** no. of words : ", colorize(Color.YELLOW, wordBB.shape[-1]))
+        # print("  ** text         : ", colorize(Color.GREEN, txt))
+
+        # if "q" in input("next? ('q' to exit) : "):
+        #     break
     db.close()
+    f.close()
 
 
 if __name__ == "__main__":
